497b567db7d1d777c953a83e9c662d8e3153592b
ramiKang <harami0007@naver.com>
2025-08-24 02:10:10 -0700

    feat: Setup Attachments

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 import fs from "fs";
+import path from "path";
+import mime from "mime-types";
 import { parse } from "csv-parse/sync";
 
 // Static Information
 const CSV_PATH = "./dataset/eng_phishing_full_information.csv";
+const ATTACHMENT_PATH_ARR = ["./dataset/test.sh"];
 
 // Load CSV
 const csvFile = fs.readFileSync(CSV_PATH, "utf-8");
@@ -16,4 +19,17 @@ const rawCSVDataArr = parse(csvFile, {
 // Remove Null Data
 const csvDataArr = rawCSVDataArr?.filter((data) => data?.To);
 
-console.log("Check csv data : ", csvDataArr[0]);
+// Setup Attachment
+const attachments = ATTACHMENT_PATH_ARR.map((attachmentPath) => {
+  const buffer = fs.readFileSync(attachmentPath);
+  const filename = path.basename(attachmentPath);
+  const contentType = mime.lookup(attachmentPath) || "application/octet-stream";
+
+  return {
+    filename,
+    contentType,
+    data: buffer,
+  };
+});
+
+console.log("attachments : ", attachments);
